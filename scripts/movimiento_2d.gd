4baf1ee1151b551bb4cbb214df7d2986ac7f2eb4
Jose Cassano <cassanojoseluis@gmail.com>
2023-07-18 01:18:11 -0300

    add galu assets and 2d character mov

--- scripts/movimiento_2d.gd
+++ scripts/movimiento_2d.gd
@@ -6,8 +6,7 @@ extends CharacterBody2D
 func _process(delta):
 	motion_ctrl()
 
-
 func motion_ctrl() -> void:
 	velocity.x = GLOBAL.get_axis().x * speed
-	velocity.y = GLOBAL.get_axis().y * speed
+	velocity.y = GLOBAL.get_axis().y * -speed
 	move_and_slide()
--- scripts/node.gd
+++ scripts/node.gd
@@ -3,20 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	initConfig()
 	print("holi")
-	TEST1.test_1()
-	incScore()
-	incScore()
-	incScore()
-	$CharacterBody2D
-
-func initConfig():
-	GLOBAL.score = 0
-
-func incScore():
-	GLOBAL.score+=1
-	print(GLOBAL.score)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
